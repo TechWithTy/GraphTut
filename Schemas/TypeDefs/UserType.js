b8fa28acc23ad68f4508761cec7c8e166a01d27e
Tyrique Daniel <37724661+TyriqueDaniel@users.noreply.github.com>
2021-06-21 22:17:41 -0600

    Reformatted file structure

---
+++ Schemas/TypeDefs/UserType.js
@@ -0,0 +1,21 @@
+const graphql = require('graphql');
+
+const { GraphQLObjectType, GraphQLInt, GraphQLString,  } = graphql;
+
+
+
+
+const UserType = new GraphQLObjectType({
+    //or Darabase Table
+    name: "UserQueryType",
+    fields: () => ({
+        id: { type: GraphQLInt },
+        firstName: { type: GraphQLString },
+        lastName: { type: GraphQLString },
+        email: { type:  GraphQLString},
+        password: { type:  GraphQLString},
+        
+    })
+})
+
+module.exports = UserType;
